e98fb94f4b1eab901edf3c6c1a1b43c7fb2ca2da
marcosdon28 <marcosdon28@gmail.com>
2022-04-04 18:25:52 -0300

    start js

---
+++ Crypto PiggyBank front/index.js
@@ -0,0 +1,18 @@
+
+// Elements
+const depositBtn = document.getElementById('btn-deposit');
+const withdrawBtn = document.getElementById('btn-withdraw');
+const connectWalletBtn = document.getElementById('connect');
+const walletLabel = document.getElementsById('wallet');
+
+function selectDepositOption(){
+
+}
+
+function connectWallet(){
+    walletLabel.style.display = 'initial';
+
+}
+
+//function usages
+connectWalletBtn.onclick = connectWallet;
